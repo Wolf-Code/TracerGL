0d5323b6fdcc923d112761b3741da822085354dd
Wolf <Wolf>
2016-06-17 01:08:26 +0200

    Attempting to fix camera.

---
+++ TracerGL/TracerGL/test.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenTK;
+using OpenTK.Graphics.OpenGL;
+
+namespace TracerGL
+{
+    class test : GameWindow
+    {
+        int vbo;
+
+        void CreateVertexBuffer( )
+        {
+            Vector3[ ] vertices = new Vector3[ 3 ];
+            vertices[ 0 ] = new Vector3( -0.1f, -1f, 0f );
+            vertices[ 1 ] = new Vector3( 1f, -1f, 0f );
+            vertices[ 2 ] = new Vector3( 0f, 1f, 0f );
+
+            GL.GenBuffers( 1, out vbo );
+            GL.BindBuffer( BufferTarget.ArrayBuffer, vbo );
+            GL.BufferData<Vector3>( BufferTarget.ArrayBuffer,
+                new IntPtr( vertices.Length * Vector3.SizeInBytes ),
+                vertices, BufferUsageHint.StaticDraw );
+        }
+
+        protected override void OnLoad( EventArgs e )
+        {
+            GL.ClearColor( Color.Brown );
+            CreateVertexBuffer( );
+        }
+
+        protected override void OnRenderFrame( FrameEventArgs e )
+        {
+            GL.Clear( ClearBufferMask.ColorBufferBit );
+
+            GL.EnableVertexAttribArray( 0 );
+            GL.BindBuffer( BufferTarget.ArrayBuffer, vbo );
+            GL.VertexAttribPointer( 0, 3, VertexAttribPointerType.Float, false, 0, 0 );
+
+            GL.DrawArrays( BeginMode.Triangles, 0, 3 );
+
+            GL.DisableVertexAttribArray( 0 );
+
+            SwapBuffers( );
+        }
+
+    }
+}
---
+++ TracerGL/TracerRenderer/ModelBuilder.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using TracerRenderer.Data;
+
+namespace TracerRenderer
+{
+    public class ModelBuilder
+    {
+        public List<Vertex> Vertices { private set; get; } = new List<Vertex>( );
+        public List<Face> Faces { private set; get; } = new List<Face>( );
+
+        public void AddVertex( Vertex v )
+        {
+            this.Vertices.Add( v );
+        }
+
+        public void AddFace( Face f )
+        {
+            this.Faces.Add( f );
+        }
+
+        public Model GetModel( )
+        {
+            return new Model( Vertices.ToArray( ), Faces.ToArray( ) );
+        }
+    }
+}
